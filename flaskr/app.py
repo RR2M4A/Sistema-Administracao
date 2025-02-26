d43ffe7f7c7eceaaa62042a93839fe1f70037513
José Victor <135292465+RR2M4A@users.noreply.github.com>
2025-02-26 16:47:19 -0300

    Configuring signin's route to enable login

--- flaskr/app.py
+++ flaskr/app.py
@@ -2,9 +2,11 @@ from flask import Flask
 from models import *
 from database import db
 
+
 def create_app():
 
     app = Flask(__name__)
+    app.secret_key = "Use a secret key here"
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///system.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
@@ -23,4 +25,4 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.run()
+    app.run(debug = True)
--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, redirect, render_template, request, session, url_for
-from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash
+from models import User
 from database import db
 
 auth = Blueprint("auth", __name__)
@@ -8,11 +9,18 @@ auth = Blueprint("auth", __name__)
 def signin_get():
     return render_template("signin.html")
 
+
 @auth.post("/signin/")
 def signin_post():
 
-    form = request.json()
-    for value in form:
-        print(value)
+    req = request.get_json()
+    inputed_username = req["username"]
+    inputed_password = req["password"]
+
+    db_user = db.session.execute(db.select(User).where(User.username == inputed_username)).scalar_one_or_none()
+
+    if db_user and check_password_hash(db_user.password, inputed_password):
+        session["id"] = db_user.id
+        return {"authenticated": "true", "redirect": url_for("main.system_get")}
     
-    return render_template("signin.html")
+    return {"authenticated": "false"}
