b7af8d314da4944a180b51f50c63b4dc219441e5
Jose <josevictor21112005@gmail.com>
2025-03-14 00:30:16 -0300

    Handling system's post requests in different endpoints
    - system/add_client endpoint to handle the creation of a client.
    - system/search_client endpoint to handle searching a client.

--- flaskr/routes/system.py
+++ flaskr/routes/system.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from utils.decorators import login_required
 from services.validation_service import ValidationService
+from services.system_service import SystemService
+from http import HTTPStatus
 
 
 system = Blueprint("system", __name__)
@@ -14,12 +16,46 @@ def system_get():
     return render_template("system.html")
 
 
-@system.post("/system/")
+@system.post("/system/add_client")
 @login_required
-def system_post():
+def system_add_client():
     """Lida com requisições do tipo POST na página principal do sistema."""
     
     req = request.get_json()
-    validation_result = ValidationService.validate_all(req)
+    errors = ValidationService.validate_all(req)
+
+    if errors:
+        return {
+            "status": "error",
+            "errors": errors
+        }, HTTPStatus.BAD_REQUEST
+
+    client = SystemService.create_client(req)
+    SystemService.create_entrance(client)
+
+    return {"status": "success"}, HTTPStatus.CREATED
+
+
+@system.post("/system/search_client")
+@login_required
+def system_search_client():
+    """Lida com requisições do tipo POST na página principal do sistema."""
+    
+    req = request.get_json()
+    cpf = req.get("cpf")
+    _, is_valid = ValidationService.validate_cpf(cpf)
+
+    if not is_valid:
+        return {"status": "error"}, HTTPStatus.BAD_REQUEST
+
+    client = SystemService.find_client(cpf)
+
+    if not client:
+        return {"status": "error"}, HTTPStatus.NOT_FOUND
+
+    SystemService.create_entrance(client)
+
+    return {"status": "success"}, HTTPStatus.OK
+
 
     
