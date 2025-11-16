a1219ea8d86f21de22e62aa8fa2e9a75afd0156e
Jose <josevictor21112005@gmail.com>
2025-11-15 22:47:51 -0300

    refactor(masks): Clean up code and add test coverage
    - Added English comments and docstrings.
    - Added unit testing for the masks.py module.
    - 'mask_client_info()' method was moved from 'system_service.py' into 'masks.py' module to ensure separation of duties (SoD).

--- flaskr/utils/masks.py
+++ flaskr/utils/masks.py
@@ -1,22 +1,45 @@
-def mask_cpf(cpf: str):
-    """Retorna o cpf mascarado."""
+from typing import Optional
+from models import Client
+from utils.converters import model_to_dict
 
-    return "{}.***.***-{}".format(cpf[:3], cpf[-2:])
+
+def mask_cpf(cpf: Optional[str]) -> str:
+    """Returns the masked CPF."""
+
+    if not cpf:
+        return "***.***.***-**"
+
+    return f"{cpf[:3]}.***.***-{cpf[-2:]}"
 
 
-def mask_rg(rg: str):
-    """Retorna o rg mascarado.
-
-    Como o RG tem diversos padrões a depender do estado, a fim de segurança
-    e praticidade, o padrão será de 7 asteriscos ('*').
+def mask_rg(rg: Optional[str]) -> str:
+    """
+    Returns the masked RG.
+    Since RG has different patterns depending on the state,
+    for security and simplicity, the pattern will be 7 asterisks.
     """
 
     return "*" * 7
 
 
-def mask_phone_number(phone_number: str):
-    """Retorna o número de telefone mascarado."""
+def mask_phone_number(phone_number: str) -> str:
+    """Returns the masked phone number."""
+
+    if not phone_number:
+        return ""
 
     if len(phone_number) == 12:
         return "({}) *****-{}".format(phone_number[:2], phone_number[-4:])
     return "({}) ****-{}".format(phone_number[:2], phone_number[-4:])
+
+
+def mask_client_info(client: 'Client') -> dict:
+    """Returns a dict of client info with sensitive data masked."""
+
+    client = model_to_dict(client)
+
+    client["cpf"] = mask_cpf(client.get("cpf"))
+    client["rg"] = mask_rg(client.get("rg"))
+    client["phone_number"] = mask_phone_number(client.get("phone_number"))
+
+    return client
