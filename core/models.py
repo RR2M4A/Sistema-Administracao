277db8d64302b9433f26aeca50bfefeaab15784d
Jose <josevictor21112005@gmail.com>
2025-12-05 00:41:02 -0300

    refactor(models): apply i18n and optimize department field
    - Implement `gettext_lazy` for all model fields and Meta verbose names to support internationalization.
    - Add department column on 'Entrance' schema.

--- core/models.py
+++ core/models.py
@@ -3,17 +3,17 @@ from django.db import models
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    pass
 
     def __str__(self):
         return self.username
 
 
 class Client(models.Model):
-    """
+    '''
     Represents the clients table in the database.
 
     Columns:
@@ -25,35 +25,67 @@ class Client(models.Model):
     - birth_date: Client's date of birth.
     - created_at: The time the object was instantiated.
     - updated_at: The time the object was updated.
-    """
+    '''
 
-    name = models.CharField("Nome", max_length=255)
-    cpf = models.CharField("CPF", max_length=14, unique=True)
-    rg = models.CharField("RG", max_length=20, blank=True, null=True)
-    phone_number = models.CharField("Telefone", max_length=20)
-    birth_date = models.DateField("Data de Nascimento", blank=True, null=True)
+    name = models.CharField(_('Nome'), max_length=255)
+    cpf = models.CharField(_('CPF'), max_length=14, unique=True)
+
+    # Rg is blank=null=True because in 2032 it won't be mandatory anymore
+    rg = models.CharField(_('RG'), max_length=20, blank=True, null=True)
+    phone_number = models.CharField(_('Telefone'), max_length=20, blank=True)
+    birth_date = models.DateField(_('Data de Nascimento'))
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
     class Meta:
-        verbose_name = "Cliente"
-        verbose_name_plural = "Clientes"
+        verbose_name = _('Cliente')
+        verbose_name_plural = _('Clientes')
 
 
     def __str__(self):
-        return f"{self.name} ({self.cpf})"
+        return f'{self.name} ({self.cpf})'
 
 
 class Entrance(models.Model):
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    created_at = models.DateTimeField("Horário de Entrada", auto_now_add=True)
+
+    class DepartmentChoices(models.TextChoices):
+        # department = value, label
+        COAG = 'COAG', 'Coordenação de Administração Geral'
+        GEAD = 'GEAD', 'Gerência de Administração'
+        NUINF = 'NUINF', 'Núcleo de Informática'
+        NUMAP = 'NUMAP', 'Núcleo de Material e Patrimônio'
+        GEOFIN = 'GEOFIN', 'Gerência de Orçamento e Finanças'
+        GEPES = 'GEPES', 'Gerência de Pessoas'
+        CODES = 'CODES', 'Coordenação de Desenvolvimento'
+        DIDOT = 'DIDOT', 'Diretoria de Desenvolvimento e Territorial'
+        GETEDEC = 'GETEDEC', 'Gerência de Gestão do Território e Desevolvimento Econômico'
+        DIART = 'DIART', 'Diretoria de Articulação'
+        GEPSCEL = 'GEPSCEL', 'Gerência de Políticas Sociais, Cultura, Esporte e Lazer'
+        COLOM = 'COLOM', 'Coordenação de Licenciamento, Obras e Manutenção'
+        DIALIC = 'DIALIC', 'Diretoria de Aprovação e Licenciamento'
+        GELOAE = 'GELOAE', 'Gerência de Licenciamento de Obras e Atividades Econômicas'
+        GEAPRO = 'GEAPRO', 'Gerência de Elaboração e Aprovação de Projetos'
+        DIROB = 'DIROB', 'Diretoria de Obras'
+        GEOB = 'GEOB', 'Gerência de Obras'
+        GEMAC = 'GEMAC', 'Gerência de Manutenção e Conservação'
+        ASCOM = 'ASCOM', 'Assessoria de comunicação'
+        ASTEC = 'ASTEC', 'Assessoria Técnica'
+        ASPLAN = 'ASPLAN', 'Assessoria de Planejamento'
+        GAB = 'GAB', 'Gabinete'
+        OUV = 'OUV', 'Ouvidoria'
+        JSM = 'JSM', 'Junta de Serviço Militar'
+
+
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name=_('Cliente'))
+    created_at = models.DateTimeField(_('Horário de Entrada'), auto_now_add=True)
+    department = models.CharField(_('Departamento'), choices=DepartmentChoices.choices)
 
 
     class Meta:
-        verbose_name = "Entrada"
-        verbose_name_plural = "Entradas"
+        verbose_name = _('Entrada')
+        verbose_name_plural = _('Entradas')
 
     def __str__(self):
-        return f"Entrada de {self.client.name} em {self.created_at}"
+        return f'Entrada de {self.client.name} em {self.created_at}'
