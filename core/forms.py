06e72114684484b43d807188712c7fec775b1bdd
Jose <josevictor21112005@gmail.com>
2026-01-23 22:30:45 -0300

    feat: add ClientEntryForm and SearchClientForm with validation logic
    - Implement 'ClientEntryForm' for registering/updating clients.
    - Add 'department' field to entry form with a default empty choice.
    - Implement validators for:
      - CPF: strips punctuation and validates digits.
      - Phone: validates against regex pattern.
      - Birth Date: prevents future dates.
    - Implement 'SearchClientForm' for the search bar, including CPF cleaning.

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,12 @@
+from datetime import date
 from django import forms
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.forms import AuthenticationForm
+from django.core.exceptions import ValidationError
+from .models import Client, Entrance
+from .regex_patterns import RegexPatterns
+from validate_docbr import CPF
+import re
 
 
 class LoginForm(AuthenticationForm):
@@ -27,3 +33,126 @@ class LoginForm(AuthenticationForm):
             'placeholder': 'Digite sua senha',
         })
     )
+
+
+class ClientEntryForm(forms.ModelForm):
+    '''
+    Represents the Client Entry Form, used to register a client when
+    he arrives at the reception.
+    '''
+
+    class Meta:
+        model = Client
+        fields = ['name', 'cpf', 'birth_date', 'phone_number']
+
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'id': 'add-name',
+                'autofocus': True,
+                'placeholder': 'Digite o nome completo',
+            }),
+            'cpf': forms.TextInput(attrs={
+                'id': 'add-cpf',
+                'placeholder': '000.000.000-00',
+            }),
+            'birth_date': forms.DateInput(attrs={
+                'id': 'add-birth-date',
+                'placeholder': 'dd/mm/aaaa',
+                'type': 'date'
+            }),
+            'phone_number': forms.TextInput(attrs={
+                'id': 'add-phone-number',
+                'placeholder': '(00) 00000-0000',
+            }),
+        }
+
+        labels = {
+            'name': _('Nome'),
+            'cpf': _('CPF'),
+            'birth_date': _('Data de Nascimento'),
+            'phone_number': _('Telefone'),
+        }
+
+
+    department = forms.ChoiceField(
+        label = _('Departamento'),
+        choices = Entrance.DepartmentChoices.choices,
+        widget=forms.Select(attrs={
+            'id': 'add-department',
+        })
+    )
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['department'].choices = [('', 'Selecione um departamento')] + list(self.fields['department'].choices)
+
+
+    def clean_phone_number(self):
+        '''Checks if the phone number is valid.'''
+
+        data = self.cleaned_data.get('phone_number', '')
+        pattern = RegexPatterns.PHONE_NUMBER.value
+
+        if not data: return data
+
+        if not re.fullmatch(pattern, data):
+            raise ValidationError(
+                _('Número de telefone inválido!')
+            )
+
+        return data
+
+
+    def clean_cpf(self):
+        '''Checks if the CPF is valid and removes punctuation.'''
+
+        data = self.cleaned_data.get('cpf', '')
+        validator = CPF()
+
+        data = re.sub(r'\D', '', data)
+
+        if not validator.validate(data):
+            raise ValidationError(_('CPF Inválido! Verifique os dígitos.'))
+
+        return data
+
+
+    def clean_birth_date(self):
+        '''Checks if the birth_date is valid. '''
+
+        birth_date = self.cleaned_data.get('birth_date')
+        today = date.today()
+
+        if not birth_date: return birth_date
+
+        if birth_date > today:
+            raise ValidationError(_('A data de nascimento não pode estar no futuro.'))
+
+        return birth_date
+
+
+class SearchClientForm(forms.Form):
+    '''
+    Represents the Search Client Form, used to look for a client
+    by his CPF.
+    '''
+
+    cpf = forms.CharField(
+        label='CPF',
+        required=False,
+        max_length=14,
+        widget=forms.TextInput(attrs={
+            'id': 'search-bar',
+            'class': 'search-bar',
+            'placeholder': 'Digite o CPF aqui',
+        })
+    )
+
+    def clean_cpf(self):
+        '''Removes punctuation from CPF.'''
+
+        data = self.cleaned_data.get('cpf', '')
+        data = re.sub(r'\D', '', data)
+
+        return data
