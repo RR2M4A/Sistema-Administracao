522e038124be0e01fa3b5f8a9f603fcf2938af09
Jose <josevictor21112005@gmail.com>
2026-01-23 22:31:37 -0300

    refactor(admin): Setting 'phone_number' as an attribute for display on admin page

--- core/admin.py
+++ core/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Client, Entrance, User, Phone
+from .models import Client, Entrance, User
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'cpf', 'birth_date', 'created_at')
-    search_fields = ('name', 'cpf')
+    list_display = ('name', 'cpf', 'phone_number', 'birth_date', 'created_at')
+    search_fields = ('name', 'cpf', 'phone_number')
     list_filter = ('created_at',)
 
 
@@ -17,9 +17,3 @@ class EntranceAdmin(admin.ModelAdmin):
     list_display = ('client', 'created_at')
     search_fields = ('client__cpf',)
     list_filter = ('created_at',)
-
-
-@admin.register(Phone)
-class PhoneAdmin(admin.ModelAdmin):
-    list_display = ('client', 'number')
-    search_fields = ('number', 'client__cpf')
