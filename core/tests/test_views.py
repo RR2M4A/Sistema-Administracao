44182c2f8cb6a8fa5cc3fe56413dece5b0bc1f23
RR2M4A <josevictor21112005@gmail.com>
2026-05-10 19:51:00 -0300

    feat: Adicionando testes para a 'CitizenDetailView', localizado em views.py

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -3,9 +3,13 @@ from django.urls import reverse
 from model_bakery import baker
 from core.models import Entrance, Status
 from django.utils import timezone
+import json
 
 @pytest.mark.django_db
 class TestEntranceListView:
+    """
+    Class responsible for holding the 'EntranceListView' tests.
+    """
 
     URL = reverse('core:main')
 
@@ -77,3 +81,95 @@ class TestEntranceListView:
         assert response.status_code == 200
         for entrance in entrances:
             assert entrance not in response.context['entrances']
+
+
+class TestCitizenDetailView:
+    """
+    Class responsible for holding the 'CitizenDetailView' tests.
+    """
+
+    URL = reverse('core:client-detail')
+
+    def test_access_without_authenticated_user(self, client):
+        """
+        Testing to access the view without an authenticated user.
+        It should return an HTML page with status 302.
+        """
+
+        response = client.get(self.URL)
+        assert response.status_code == 302
+
+
+    def test_with_valid_query(self, logged_in_client):
+        """
+        Testing for when a client exists, the payload is valid and the CPF is valid.
+        It should return a JSON response with status 200 and the client's information.
+        """
+
+        target_citizen = baker.make(
+            'core.Citizen',
+            name="John Doe Smith",
+            cpf="13610361093",
+            birth_date="1985-12-25",
+            phone_number="61912345678",
+            status=Status.REGULAR
+        )
+        
+        json_response = logged_in_client.post(
+            self.URL,
+            data=json.dumps({'cpf': '136.103.610-93'}),
+            content_type='application/json'
+        )
+
+        response_content = json_response.json()
+
+        assert json_response.status_code == 200, json_response
+        assert response_content['type'] == 'success'
+        assert response_content['dict']['name'] == "John Doe Smith"
+        assert response_content['dict']['birth_date'] == "25/12/1985"
+        assert response_content['dict']['phone_number'] == "(61) 91234-5678"
+
+
+    def test_without_an_existing_client(self, logged_in_client):
+        """
+        Testing for when the payload is valid, the CPF is valid, but the client doesn't exist.
+        It should return a JSON response with status 404.
+        """
+        
+        json_response = logged_in_client.post(
+            self.URL,
+            data=json.dumps({'cpf': '136.103.610-93'}),
+            content_type='application/json'
+        )
+
+        assert json_response.status_code == 404
+
+
+    def test_with_an_invalid_cpf(self, logged_in_client):
+        """
+        Testing for when the payload is valid, the client exists, but the CPF is invalid.
+        It should return a JSON response with status 400.
+        """
+        
+        json_response = logged_in_client.post(
+            self.URL,
+            data=json.dumps({'cpf': '000.000.000-00'}),
+            content_type='application/json'
+        )
+
+        assert json_response.status_code == 400
+
+
+    def test_with_an_invalid_payload(self, logged_in_client):
+        """
+        Testing for when the client exists, the CPF is valid, but the payload is invalid.
+        It should return a JSON response with status 400.
+        """
+        
+        json_response = logged_in_client.post(
+            self.URL,
+            data="not_a_json_format",
+            content_type='application/json'
+        )
+
+        assert json_response.status_code == 400
